3eb972341256cce3d478b8bf2fd65cec6c3472c7
Wimps <mathieucorti@gmail.com>
2018-01-30 09:08:16 +1100

    Added basic reaction #chicken

--- louis.js
+++ louis.js
@@ -12,13 +12,27 @@ client.on('ready', () => {
 bot.create(function (err, session) {
   console.log("Cleverbot logged !");
   client.on('message', msg => {
-    if(msg.author.id !== bot.user.id && msg.isMentioned(client.user)) {
-     const cleanMessage = msg.content.replace(/(<.*?>|@.*?)(?: |\s)/g, "");
-      bot.ask(cleanMessage, function (err, response) {
-        msg.channel.send(err ? "An error has occurred :(" : response);
-      });
+    if(msg.author.id !== bot.user.id) {
+      answer(msg);
     }
   });
 });
 
-client.login('NDA3NDQ3ODE0MjIyNzc0Mjcy.DVBpKA.6zup2uPxyiigiHsZYfTnztVf1N4');
+// If it's not the bot itself
+function answer(msg) {
+  if (msg.isMentioned(client.user)) {
+    const cleanMessage = msg.content.replace(/(<.*?>|@.*?)(?: |\s)/g, "");
+    bot.ask(cleanMessage, function (err, response) {
+      msg.channel.send(err ? "An error has occurred :(" : response);
+    });
+  }
+  if (msg.cleanContent.indexOf("chicken") > -1) {
+    msg.react("🐔").catch(function () {
+      console.log("Failed to react to [" + msg.cleanContent + "].");
+    });
+  }
+}
+
+client.login('NDA3NDQ3ODE0MjIyNzc0Mjcy.DVBpKA.6zup2uPxyiigiHsZYfTnztVf1N4').catch(function () {
+  console.log("Failed to log to Discord.");
+});
