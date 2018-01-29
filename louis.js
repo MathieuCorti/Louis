150f2e1953c0dab474f327aabb24ff44124ba21d
Wimps <mathieucorti@gmail.com>
2018-01-29 20:31:29 +1100

    Louis is alive

---
+++ louis.js
@@ -0,0 +1,24 @@
+const Discord   = require("discord.js");
+const Cleverbot = require("cleverbot.io");
+
+const client  = new Discord.Client();
+const bot     = new Cleverbot("Bfa7D3rlzjl3imaH", "KFWAdnsfyoDIRqyxMfZu3mWgwwkr5fus");
+bot.setNick("Louis");
+
+client.on('ready', () => {
+  console.log("Logged in as " + client.user.tag + "!");
+});
+
+bot.create(function (err, session) {
+  console.log("Cleverbot logged !");
+  client.on('message', msg => {
+    if(msg.author.id != bot.user.id && msg.isMentioned(client.user)) {
+     const cleanMessage = msg.content.replace(/(<.*?>|@.*?)(?: |\s)/g, "");
+      bot.ask(cleanMessage, function (err, response) {
+        msg.channel.send(response);
+      });
+  }
+});
+});
+
+client.login('NDA3NDQ3ODE0MjIyNzc0Mjcy.DVBpKA.6zup2uPxyiigiHsZYfTnztVf1N4');
