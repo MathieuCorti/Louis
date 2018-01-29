5d7814770a80caebdcd1c61cabe224672a24afaa
Wimps <mathieucorti@gmail.com>
2018-01-30 09:45:48 +1100

    Added gif support, but not very good results :(

--- louis.js
+++ louis.js
@@ -1,5 +1,6 @@
 const Discord   = require("discord.js");
 const Cleverbot = require("cleverbot.io");
+const Giphy = require('giphy-api')();
 
 const client  = new Discord.Client();
 const bot     = new Cleverbot("Bfa7D3rlzjl3imaH", "KFWAdnsfyoDIRqyxMfZu3mWgwwkr5fus");
@@ -12,23 +13,32 @@ client.on('ready', () => {
 bot.create(function (err, session) {
   console.log("Cleverbot logged !");
   client.on('message', msg => {
-    if(msg.author.id !== bot.user.id) {
+    if(!msg.author.bot) {
       answer(msg);
     }
   });
 });
 
-// If it's not the bot itself
 function answer(msg) {
+  // If the bot is mentioned, respond with the cleverbot.io API
   if (msg.isMentioned(client.user)) {
     const cleanMessage = msg.content.replace(/(<.*?>|@.*?)(?: |\s)/g, "");
     bot.ask(cleanMessage, function (err, response) {
       msg.channel.send(err ? "An error has occurred :(" : response);
     });
   }
-  if (msg.cleanContent.indexOf("chicken") > -1) {
+  // React with 🐔 if the message contains "chicken"
+  if (msg.cleanContent.toLocaleLowerCase().indexOf("chicken") > -1) {
     msg.react("🐔").catch(function () {
       console.log("Failed to react to [" + msg.cleanContent + "].");
+    });
+  }
+  // Send a gif if the message contains only one word
+  if (!/\s/g.test(msg)) {
+    Giphy.random(msg, function (err, res) {
+      if (err == null) {
+        msg.channel.send(res.data.url);
+      }
     });
   }
 }
